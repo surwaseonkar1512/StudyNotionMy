947f59c42d30d5a38e73cde3dbac89da99934812
Surwase Onkar <118064112+surwaseonkar1512@users.noreply.github.com>
2023-07-15 22:47:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://study-notion-my2.vercel.app/",
+    origin: "https://study-notion-my2.vercel.app",
     credentials: true,
   })
 );
